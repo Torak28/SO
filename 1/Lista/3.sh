5c223831d03e08a94e1508e0f5f2561b5ea6ab07
Torak28 <zelechowski28@gmail.com>
2017-03-12 00:12:30 +0100

    Usuwanie bangla

---
+++ 1/Lista/3.sh
@@ -0,0 +1,14 @@
+#! /bin/bash/
+
+#Kasowanie  wszystkich  plików  w  zadanym  katalogu  (parametr  wywołania  skryptu),  poza  
+#plikami wykonywalnymi, mającymi ustawiony bit dostępu typu „execute”.
+
+echo Usuwanie plikow w $1 jeśli nie są "execute"
+
+for file in $1/*; do
+	if [ -x $file -a -f $file ]
+	then
+		echo Usuwanie $file
+		rm $file
+	fi
+done 
