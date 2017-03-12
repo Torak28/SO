21d07cf43bad8473b6315ff57a3eb20e809eb29c
Torak28 <zelechowski28@gmail.com>
2017-03-12 19:55:41 +0100

    Dodany podwojny for i sprawdzanie, obylo sie bez flag

--- 1/Lista/10.sh
+++ 1/Lista/10.sh
@@ -6,4 +6,14 @@
 
 echo Wypisanie plikow o identycznych nazwach w katologu: $1 i $2
 
-#TODO Jakaś zabawa forami z jakas flaga oznaczenia
+for fileA in `ls $1`; do
+	for fileB in `ls $2`; do
+		if [ -f "$1"/$fileA -a -f "$2"/$fileB -a $fileA = $fileB ]
+		then
+			echo $fileA jest w katalogu $1 i $2
+		fi
+	done 
+done
+
+#Komendy
+# $A = $B - jako sprawdzenie czy są takie same nazwy
