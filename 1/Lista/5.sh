7fe3523b9aa1d1d5a882c2ae1fc4cb15bb6b68cd
Torak28 <zelechowski28@gmail.com>
2017-03-12 11:59:26 +0100

    Agregacja w pliku zrobiona

---
+++ 1/Lista/5.sh
@@ -0,0 +1,25 @@
+#! /bin/bash/
+
+#Łączenie w jeden wszystkich plików należących do zadanego katalogu (parametr wywołania 
+#skryptu), o zadanym rozszerzeniu (drugi parametr skryptu). Kolejność, w jakiej pliki zostaną
+#połączone  -  nieistotna.  Treść  każdego  pliku  powinna  być  poprzedzona  nagłówkiem  z  jego  
+#nazwą
+
+echo Łącze pliki z $1 w jeden plik tekstowy
+
+#Tworzenie pliczku:
+PLICZKI=$1/pliczki.txt
+touch $PLICZKI
+
+for file in $1/*;do
+	if [ -f $file -a $file != $PLICZKI ]
+	then
+		echo Zapisywanie $file
+		echo $file >> $PLICZKI
+		cat $file >> $PLICZKI
+		echo "" >> $PLICZKI
+	fi
+done
+
+#Komendy:
+# >> jako przekazanie strumienia 
