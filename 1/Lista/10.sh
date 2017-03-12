04326570e7cfb44ac15e6c191ab88884b885d888
Torak28 <zelechowski28@gmail.com>
2017-03-12 15:01:16 +0100

    Do zrobienia

---
+++ 1/Lista/10.sh
@@ -0,0 +1,9 @@
+#! /bin/bash/
+
+#Porównanie  zawartości  dwóch  zadanych  katalogów  (argumenty  skryptu).  Przy  porównaniu  
+#należy ignorować podkatalogi. W wyniku wyświetlić na ekranie listę plików o identycznych 
+#nazwach w obu katalogach. 
+
+echo Wypisanie plikow o identycznych nazwach w katologu: $1 i $2
+
+#TODO Jakaś zabawa forami z jakas flaga oznaczenia
